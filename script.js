f86db941186308564865b1a3fc83eca88f114ba1
Diego S <diegom5332@gmail.com>
2026-03-04 14:08:39 -0300

    commit final

---
+++ script.js
@@ -0,0 +1,77 @@
+// comparação por igualdade 
+// java ==, python: ==
+// javascript: == ou ===(usamos apenas esse)
+//null e undefined
+//exemplos:
+//a=null
+//b=undefined
+// lista = []
+// lista2 = lista
+// console.log(lista2 == lista)
+// console.log(false == [])
+// console.log(null==undefined) : true
+// console.log(null == null) : true
+// console.log(1 === [1]) 
+// console.log(true == 1) :true
+// console.log(1 === '1') : false
+// console.log(1 == '1') : true
+// console.log(1 === 1) : true 
+// console.log(1 == 1):true
+//coerção explicita
+//const n1 = 2
+//const n2 = '3'
+//const n3 = n1 +Number(n2)
+//console.log(`$${n1} + ${n2} = ${n3}`)
+//coerção implicita
+//const n1 = 2
+//const n2 = '3'
+//const n3 = n1 + n2
+//console.log(`${n1} + ${n2} = ${n3}´)
+
+//declaração de variáveis
+//const,let e var
+//null undefined
+//tipos
+//js `dinamicamente tipa
+//let a = 2
+//console.log(typeof(a))
+//a = true 
+//console.log(typeof(a))
+
+//s = "abc"
+//String s = "abc";
+
+//listaDeLista[ [], [] , [],2]
+
+//var idade = 18;
+//var nome = 'Maria'
+
+//console.log(`oi, ${nome}`)
+
+//if (idade >= 18) {
+    //console.log(`parabéns, ${nome}. Você pode dirigir.`)
+  //  var nome = 'Maria'
+//}
+
+//console.log("até mais, " + nome + '.')     
+
+//var linguagem = 'javascript'
+//console.log(`aprendendo ${linguagem}`)
+ 
+//linguagem = 'java'
+//console.log(`aprendendo ${linguagem}`)
+
+
+//console.log('antes do for')
+//for (const i = 0;i<10;i = i+1) {
+//    console.log('for com const...')
+//}
+//console.log('depois do for ')
+//let nome = 'Maria'
+
+//console.log(nome)
+// nome = `Meu nome é ${nome}.`
+//console.log(nome)
+
+
+//console.log("Hello, JS")
