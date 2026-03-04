37c30274833d1eee794959329c32fb728c167b7a
Diego S <diegom5332@gmail.com>
2026-03-04 16:09:03 -0300

    learn: encerra codigo

--- script.js
+++ script.js
@@ -1,3 +1,75 @@
+//funções
+//arrow function
+const dobrar = n => (2 * n)
+console.log(dobrar(6))
+const triplicar = (n) => {
+  console.log("Vamos calcular o tripo de " +n)
+  return 3 * n;
+}
+//escreva uma função que decida se um valor é par ou não
+const par = (numero)=> numero %2 === 0
+// const hello  = (nome) => {console.log('Oi, ' + nome)}
+// hello('ana')
+// const hello  = () => {console.log('Oi ')}
+// hello()
+// const dobrar = function(n) {
+//   return 2 * n
+// }
+// console.log(dobrar(2))
+// console.log(dobrar(undefined))
+
+// console.log(triplicar(4))
+// console.log(triplicar(undefined))
+
+// const triplicar = function (n=5 ) {
+//   return 3 * n
+// }
+// const  resultado = triplicar(10)
+// console.log(resultado)
+// console.log(triplicar(undefined))
+
+// const produto = function (a,b) {
+//   console.log(a*b)
+// }
+// produto
+
+// const triplicar = function (n=5) {
+//   return 3 * n
+// }
+// function somar (a,b) {
+//   return a + b
+
+// }
+// console.log(somar(2,3))
+
+//functions e arrow functions
+// function hello() {
+//   console.log('Oi')
+// }
+// hello()
+// function hello (nome) {
+//   console.log('Oi,'  + nome)
+// }
+// hello('Ana')
+
+
+
+
+// v1 = []
+// console.log(v1.length) // 0
+
+// v1[0] = 3.4
+// console.log(v1.length) // 1
+
+// v1[10] = 'abc'
+// console.log(v1.length) // 11
+
+// console.log(v1)
+
+// for(let i = 0;i< v1.length;i++) {
+//    console.log(v1[i])
+// }
+
 // comparação por igualdade 
 // java ==, python: ==
 // javascript: == ou ===(usamos apenas esse)
